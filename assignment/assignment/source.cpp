3603088ce74a6ec7f5dd305e7f7de27861b0095d
eunhee <dmsgml9903@gmail.com>
2021-11-11 14:23:24 +0900

    9주차

--- assignment/assignment/source.cpp
+++ assignment/assignment/source.cpp
@@ -1,51 +1,115 @@
 #include <iostream>
 using namespace std;
 
-class Complex
+class Date
 {
-	// 필요한 멤버 구성
 public:
-	friend Complex add(Complex, Complex);
-	Complex(double r, double i) { re = r; im = i; }
-	Complex(double r) { re = r; im = i; }
-	Complex() { re = im = 0; }
-	void Ouput() {
-		cout << re << " + " << im << "i" << endl;
-	}
+	int year;
+	int month;
+	int day;
 
-	ostream& operator<<(ostream& os) {
-		os << this->re << " + " << this->im << "i" << endl;
-		return os;
+	Date() {
+		year = 0;
+		month = 0;
+		day = 0;
 	}
-private:
-	double re, im;
+	Date(int y, int m, int d) {
+		year = y;
+		month = m;
+		day = d;
+	}
+	~Date() {}
+
+	Date operator++() {
+
+		Date d(0, 0, 0);
+
+		d.day = this->day;
+		d.month = this->month;
+		d.year = this->year;
+
+		if (d.day == 31) {
+			if (d.month == 12) {
+				d.day = 1; d.month = 1; d.year += 1;
+			}
+			else {
+				d.day = 1; d.month += 1; //year = this->year;
+			}
+		}
+		else {
+			d.day += 1; //month = this->month; year = this->year;
+		}
+
+		return d;
+	}
 };
+ostream& operator<<(ostream& os, const Date& d) {
+	os << d.year << "년" << d.month << "월" << d.day << "일" << endl;
+	return os;
+}
+int main() {
+	Date d1(2020, 12, 31);
+	cout << d1;
+	cout << "++ 연산자 : ";
+	//++d1; // d1.operator++()
+	cout << ++d1;
 
-// 필요한 전역 함수 정의
-Complex add(Complex a1, Complex a2) {
-	return Complex(a1.re + a2.re, a1.im + a2.im);
+	cout << "한 번 더 ++ 연산자 : ";
+	cout << ++d1;
+	/*Date d2(2020, 12, 31);
+	cout << d2;
+	cout << "-- 연산자 : ";
+	cout << --d2;
+	cout << endl;*/
+
+	/*Date d3(2021, 1, 1);
+	cout << d3;
+	cout << "++ 연산자 : ";
+	cout << ++d3;*/
+	/*Date d4(2021, 1, 1);
+	cout << d4;
+	cout << "-- 연산자 : ";
+	cout << --d4;*/
+	return 0;
 }
 
-
-
-int main()
-{
-	Complex c1(1, 2), c2(3, 4);
-	Complex c3 = c1 + c2;
-
-	c1 << cout;
-
-	cout << c3 << endl;
-
-	c3 = c3 + 1;
-	cout << c3 << endl;
-
-	cout << "c1, c2, c3 : " << endl;
-	c3 << (c2 << (c1 << cout));
-	cout << endl;
-
-	cout << "c1, c2, c3 : " << endl;
-	cout << c1 << c2 << c3 << endl; // Ostream& operator+(Ostream& c1, Complex& c2)
-	// cout.operator+(c1)
-	return(0);
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class Employee {
+//private:
+//	string name;
+//	int salary;
+//public:
+//	Employee() {
+//		name = "Unknown";
+//		salary = 0;
+//	}
+//	Employee(string name, int salary)
+//	{
+//		this->name = name;
+//		this->salary = salary;
+//	}
+//
+//	operator int() { return salary; }
+//};
+//
+//int main()
+//{
+//	Employee e1("홍길동", 100);
+//	Employee e2("홍길서", 110);
+//
+//	int salary = e1;
+//	cout << "홍길동의 salary = " << salary << endl;
+//
+//	cout << "홍길서의 salary = " << e2 << endl;
+//
+//	if (e1 > e2)
+//		cout << "홍길동의 급여가 높다" << endl;
+//	else
+//		cout << "홍길서의 급여가 높다" << endl;
+//
+//	return 0;
+//}
